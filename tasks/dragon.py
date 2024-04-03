6817d0f9d77524283fef60f4926327f1d40b3aed
asdergil <160749064+asdergil@users.noreply.github.com>
2024-04-03 13:57:24 +0000

    dragon_elso

--- tasks/dragon.py
+++ tasks/dragon.py
@@ -3,11 +3,15 @@ def dragon_solution(is_dead, number_of_cows):
     thin_alive_cow_index = number_of_cows - 1
     
     while (fat_alive_cow_index + 1) < thin_alive_cow_index:
-        middle_cow = int((fat_alive_cow_index + 
-                          thin_alive_cow_index) / 2)
+        middle_cow = fat_alive_cow_index + (thin_alive_cow_index-fat_alive_cow_index)//2
         if is_dead(middle_cow):
             thin_alive_cow_index = middle_cow
         else:
             fat_alive_cow_index = middle_cow
     
-    return middle_cow
+    if is_dead(thin_alive_cow_index):
+        return fat_alive_cow_index
+    else:
+        return thin_alive_cow_index
+
+    
